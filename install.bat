e706c97690aa99ff841eb96878bd6a24cbb4e2dd
Pommepoirechocolat <85391741+pommepoirechocolat@users.noreply.github.com>
2021-06-20 16:47:54 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,22 @@
+@echo off 
+echo Installation des module...
+timeout /t 5
+color 5
+pip install webbrowser
+timeout /t 1
+pip install requests
+timeout /t 1
+pip install BeautifulSoup
+timeout /t 1
+pip install discord
+timeout /t 1
+pip install DiscordWebhook
+timeout /t 1
+pip install DiscordEmbed
+timeout /t 1
+pip install requests
+timeout /t 1
+
+echo appuie sur une touche pour continuer ou le programe se ferme automatiquement dans 10 seconde...
+
+
